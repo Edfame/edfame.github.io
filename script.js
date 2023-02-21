36535cbd6fe8a66c66caa33a7102d9a3ee15765b
Eduardo Farófia Medeiros <edfame@gmail.com>
2023-02-21 18:32:11 +0000

    images/

--- script.js
+++
@@ -1,8 +0,0 @@
-function content() {
-
-    document.getElementById("content").innerHTML = "<a href=\"https://github.com/Edfame\"><img src=\"images/git.png\"></a>\n" +
-                                                    "<a href=\"https://www.linkedin.com/in/edfame/\"><img src=\"images/linkedin.png\"></a>"+
-                                                    "<a href =\"https://www.twitter.com/Edfame_\"><img src=\"images/twitter.png\"></a>"+
-                                                    "<a href=\"https://www.instagram.com/edfame_/\"><img src=\"images/insta.png\"></a>"
-}
-
